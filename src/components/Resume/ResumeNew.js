22aca4f46cbab4252e88b65326f7b12d0ebf514c
unknown <gurshaan7869@gmail.com>
2023-01-15 20:51:36 +0530

    commit

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/gurshaan7869/Digital-Portfolio/master/src/Assets/Gurshaan%20resume.pdf";
+  "https://raw.githubusercontent.com/gurshaan7869/Digital-Portfolio/master/src/Assets/resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
